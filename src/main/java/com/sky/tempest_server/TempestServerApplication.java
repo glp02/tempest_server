60a77e334580e783e7826c14ec69e0f2ffbd06b4
Gianmarco Luppi <gianmarco.luppi@sky.uk>
2022-07-22 16:29:25 +0100

    TMP-1 first commit

---
+++ src/main/java/com/sky/tempest_server/TempestServerApplication.java
@@ -0,0 +1,13 @@
+package com.sky.tempest_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempestServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sky/tempest_server/TempestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.tempest_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
