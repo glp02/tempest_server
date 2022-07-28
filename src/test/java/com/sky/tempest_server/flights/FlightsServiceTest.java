df688fa7f1ab877e9d5db6f655dcf4436c5b3ab5
Gianmarco Luppi <gianmarco.luppi@sky.uk>
2022-07-28 09:18:37 +0100

    adds flights service test file

---
+++ src/test/java/com/sky/tempest_server/flights/FlightsServiceTest.java
@@ -0,0 +1,9 @@
+package com.sky.tempest_server.flights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FlightsServiceTest {
+    
+}
