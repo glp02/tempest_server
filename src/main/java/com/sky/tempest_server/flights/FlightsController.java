bec9dc74d73d3758cd13da267596c0118efbdd1a
ollie croysdill <oliver.croysdill@sky.uk>
2022-08-01 12:14:13 +0100

    adds functionality to get list of city locations, and search for flights by city code

--- src/main/java/com/sky/tempest_server/flights/FlightsController.java
+++ src/main/java/com/sky/tempest_server/flights/FlightsController.java
@@ -1,7 +1,7 @@
 package com.sky.tempest_server.flights;
 
-import com.sky.tempest_server.flights.entities.Airport;
 import com.sky.tempest_server.flights.entities.Flight;
+import com.sky.tempest_server.flights.entities.Location;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,31 +9,47 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 public class FlightsController {
-    private FlightsService service;
+    private final FlightsService service;
 
     @Autowired
     public FlightsController(FlightsService service) {
         this.service = service;
     }
 
-    @GetMapping("/flights/search-airports")
-    public List<Airport> searchAirports(@RequestParam(name="searchText") String searchText) throws IOException {
-        return this.service.searchAirports(searchText);
+    @GetMapping("/flights/search-cities")
+    public List<Location> searchCities(@RequestParam(name="searchText") String searchText) throws IOException {
+        return this.service.searchLocations("city", searchText);
     }
 
+    @GetMapping("/flights/search-airports")
+    public List<Location> searchAirports(@RequestParam(name="searchText") String searchText) throws IOException {
+        return this.service.searchLocations("airport", searchText);
+    }
 
-    @GetMapping("/flights/search-flights")
-    public List<Flight> searchFlights(@RequestParam(name="flightDate") String flightDate,
+    @GetMapping("/flights/search-locations")
+    public List<Location> searchLocations(@RequestParam(name="searchText") String searchText) throws IOException {
+        List<Location> cityList =  this.searchCities(searchText);
+        List<Location> airportList =  this.searchAirports(searchText);
+        return Stream.concat(cityList.stream(), airportList.stream()).collect(Collectors.toList());
+    }
+
+    @GetMapping("/flights/search-flights-by-airport-code")
+    public List<Flight> searchFlightsByAirport(@RequestParam(name="flightDate") String flightDate,
                                       @RequestParam(name="departureAirportCode") String departureAirportCode,
                                       @RequestParam(name="arrivalAirportCode") String arrivalAirportCode) throws IOException {
-        return this.service.searchFlights(flightDate, departureAirportCode, arrivalAirportCode);
+        return this.service.searchFlights("airport",flightDate, departureAirportCode, arrivalAirportCode);
     }
 
-
-
-
+    @GetMapping("/flights/search-flights-by-city-code")
+    public List<Flight> searchFlightsByCity(@RequestParam(name="flightDate") String flightDate,
+                                      @RequestParam(name="departureCityCode") String departureCityCode,
+                                      @RequestParam(name="arrivalCityCode") String arrivalCityCode) throws IOException {
+        return this.service.searchFlights("city",flightDate, departureCityCode, arrivalCityCode);
+    }
 
 }
--- src/main/java/com/sky/tempest_server/flights/FlightsService.java
+++ src/main/java/com/sky/tempest_server/flights/FlightsService.java
@@ -6,13 +6,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
 import com.sky.tempest_server.flights.entities.Airport;
 
+import com.sky.tempest_server.flights.entities.City;
 import com.sky.tempest_server.flights.entities.Flight;
+import com.sky.tempest_server.flights.entities.Location;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,11 +34,11 @@ public class FlightsService {
         this.tequilaAPIService = tequilaAPIService;
     }
 
-    public List<Airport> searchAirports(String searchText) throws IOException {
+    public List<Location> searchLocations(String locationType, String searchText) throws IOException {
         //BUILD URL WITH QUERY PARAMETERS
         String queryUrlParams = UriComponentsBuilder.fromPath("")
                 .queryParam("term", searchText)
-                .queryParam("location_types", "airport")
+                .queryParam("location_types", locationType)
                 .encode()
                 .toUriString();
 
@@ -46,22 +49,31 @@ public class FlightsService {
         JsonNode locationsJSON = responseJSON.get("locations");
         List<JsonNode> locationsList = readJsonArrayToJsonNodeList.readValue(locationsJSON);
 
-        return locationsList.stream().map((locationNode) -> new Airport(
-                locationNode.get("name").textValue(),
-                locationNode.get("code").textValue(),
-                locationNode.get("city").get("name").textValue(),
-                locationNode.get("city").get("country").get("name").textValue()
+        if(locationType.equals("city")) {
+            return locationsList.stream().map((locationNode) -> new City(
+                    locationNode.get("name").textValue(),
+                    locationNode.get("country").get("name").textValue(),
+                    locationNode.get("code").textValue()
+            )).filter(city -> (city.getCityCode() != null)).collect(Collectors.toList());
+        }
+        if(locationType.equals("airport")) {
+            return locationsList.stream().map((locationNode) -> new Airport(
+                    locationNode.get("name").textValue(),
+                    locationNode.get("city").get("country").get("name").textValue(),
+                    locationNode.get("city").get("name").textValue(),
+                    locationNode.get("code").textValue()
             )).collect(Collectors.toList());
+        }
+        else return new ArrayList<>();
     }
 
-
-    public List<Flight> searchFlights(String flightDate, String departureAirportCode, String arrivalAirportCode) throws IOException {
+    public List<Flight> searchFlights(String locationType, String flightDate, String departureAirportCode, String arrivalAirportCode) throws IOException {
         //BUILD URL WITH QUERY PARAMETERS
         String queryUrlParams = UriComponentsBuilder.fromPath("")
                 .queryParam("date_from", flightDate)
                 .queryParam("date_to", flightDate)
-                .queryParam("fly_from", departureAirportCode)
-                .queryParam("fly_to", arrivalAirportCode)
+                .queryParam("fly_from", locationType+":"+departureAirportCode)
+                .queryParam("fly_to", locationType+":"+arrivalAirportCode)
                 .queryParam("max_stopovers", 0)
                 .encode()
                 .toUriString();
@@ -86,5 +98,10 @@ public class FlightsService {
                 flightNode.get("route").get(0).get("airline").textValue()
         )).collect(Collectors.toList());
     }
+
+
+
+
+
 }
 
