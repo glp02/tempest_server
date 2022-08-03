d1028444f8582f1c5f2cfba80721781c70397b5c
Gianmarco Luppi <gianmarco.luppi@sky.uk>
2022-08-03 10:45:41 +0100

    adds repo annotation to flights repo

--- src/main/java/com/sky/tempest_server/flights/repositories/FlightsRepository.java
+++ src/main/java/com/sky/tempest_server/flights/repositories/FlightsRepository.java
@@ -2,6 +2,8 @@ package com.sky.tempest_server.flights.repositories;
 
 import com.sky.tempest_server.flights.entities.Journey;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FlightsRepository extends CrudRepository<Journey,Long> {
 }
